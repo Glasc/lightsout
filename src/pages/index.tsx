54840c71482190be694ae5b29adfd12ef824a7fb
Glasc <63518997+Glasc@users.noreply.github.com>
2023-08-28 00:53:33 -0600

    new colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import type { NextPage } from "next"
 import styles from "../styles/Home.module.scss"
 import { Heading } from "../components/heading/heading"
 import { Grid } from "../components/grid/grid"
+import ReactMarkdown from "react-markdown"
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.game}>
-        <Heading />
-        <Grid />
-      </div>
+      <Heading />
+      <Grid />
     </div>
   )
 }
